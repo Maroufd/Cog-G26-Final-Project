afc4c2c37bfd3d6df387ecf15f1cc9cb4c8dbcf2
Maroufd <maroufdani@gmail.com>
2020-11-01 13:36:19 +0100

    Update main.py
    lvqnet added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from data_utils import extract_X_y
 from mlp import make_mlp
-
+from lvq import train_lvq
 
 if __name__ == "__main__":
     X, y, preprocessor = extract_X_y("GOOD_histograms")
     model = make_mlp(X, y)
+    lvqnet= train_lvq(X,y)
