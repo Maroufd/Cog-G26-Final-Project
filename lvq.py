58ce7b1b56a608bedb4e00b4e31d56de3aa27b40
Swopper050 <robocop@brams-mbp.home>
2020-11-03 12:53:10 +0100

    Naming convention

--- lvq.py
+++ lvq.py
@@ -1,15 +1,12 @@
-import numpy as np
 from neupy import algorithms
 
-def train_lvq(X,y):
-     """
-   Without testing just the base
+
+def train_lvq(X, y):
+    """
+    Without testing just the base
     """
 
     lvqnet = algorithms.LVQ(n_inputs=3870, n_classes=10)
-    trained_model=lvqnet.train(X, y, epochs=100)
-
+    trained_model = lvqnet.train(X, y, epochs=100)
 
     return trained_model
-
-
--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from mlp import make_mlp
 from lvq import train_lvq
 
 if __name__ == "__main__":
-    X, y, preprocessor = extract_X_y("GOOD_histograms")
+    X, y, preprocessor = extract_X_y("GOOD_washington")
     model = make_mlp(X, y)
-    lvqnet= train_lvq(X,y)
+    # lvqnet= train_lvq(X,y)
