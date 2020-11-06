a10a14688f21f7d3d6776e605db2461582e401e9
Swopper050 <robocop@brams-mbp.home>
2020-11-06 12:06:32 +0100

    k_fold experiment ready

--- k_fold_main.py
+++ k_fold_main.py
@@ -1,7 +1,5 @@
-from collections import defaultdict
 import numpy as np
-import pprint
-import time
+import pandas as pd
 
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import accuracy_score
@@ -12,30 +10,22 @@ from lvq import make_lvq
 from knn import make_knn
 
 if __name__ == "__main__":
-    X, y, preprocessor = extract_X_y("GOOD_washington")
+    X, y, preprocessor = extract_X_y("GOOD_restaurant")
 
     strat_split = StratifiedShuffleSplit(n_splits=10, test_size=0.1, random_state=42)
 
-    results = defaultdict(list)
-    for train_idx, test_idx in strat_split.split(X, np.argmax(y, axis=-1)):
-        start = time.time()
+    results = []
+    for i, (train_idx, test_idx) in enumerate(strat_split.split(X, np.argmax(y, axis=-1))):
         X_train, y_train = X[train_idx], y[train_idx]
         X_test, y_test = X[test_idx], y[test_idx]
         y_classes = np.argmax(y_test, axis=-1)
 
-        print("training knn")
         knn = make_knn(X_train, y_train)
-        print("training mlp")
         mlp = make_mlp(X_train, y_train)
-        print("training lvq")
         lvq = make_lvq(X_train, y_train)
 
-        results["knn"].append(accuracy_score(np.argmax(knn.predict(X_test), axis=-1), y_classes))
-        results["mlp"].append(accuracy_score(np.argmax(mlp.predict(X_test), axis=-1), y_classes))
-        results["lvq"].append(accuracy_score(lvq.predict(X_test), np.argmax(y_test, axis=-1)))
+        results.append({"run": i, "model": "knn", "score": accuracy_score(np.argmax(knn.predict(X_test), axis=-1), y_classes)})
+        results.append({"run": i, "model": "knn", "score": accuracy_score(np.argmax(mlp.predict(X_test), axis=-1), y_classes)})
+        results.append({"run": i, "model": "knn", "score": accuracy_score(lvq.predict(X_test), y_classes)})
 
-        pprint.pprint(results)
-        end = time.time()
-        import pdb; pdb.set_trace()
-
-    import pdb; pdb.set_trace()
+    pd.DataFrame(results).to_csv("k_fold_results.csv")
