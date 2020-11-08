ce78eaddf342bc15f1a270588cf7e3e4f7d28f5c
Swopper050 <robocop@brams-mbp.home>
2020-11-08 09:25:25 +0100

    Results + knn fix

--- open_ended_main.py
+++ open_ended_main.py
@@ -34,8 +34,8 @@ def add_old_cats(dataset, old_classes, min_cats, cls_X, cls_y):
 
 
 def open_ended_experiment():
-    type = "mlp"
-    dataset = "GOOD_washington"
+    type = "knn"
+    dataset = "ESF_washington"
     test_size = .2
     X, y, preprocessor = extract_X_y(dataset)
     original_y = preprocessor.inverse_transform(y)
